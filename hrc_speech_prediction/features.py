c9047294f2acd720e9604dc083560a809f2c0e39
Olivier Mangin <olivier@mangin.com>
2017-10-04 09:32:34 -0400

    [train] Adds bootstrap utterances for missing table parts.

--- hrc_speech_prediction/features.py
+++ hrc_speech_prediction/features.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 
-def get_bow_features(data, tfidf=True, n_grams=(1,1), max_features=None):
+def get_bow_features(data, tfidf=True, n_grams=(1, 1), max_features=None):
     if tfidf:
         vectorizer = TfidfVectorizer()
     else:
@@ -10,7 +10,6 @@ def get_bow_features(data, tfidf=True, n_grams=(1,1), max_features=None):
                                      max_features=max_features)
     X = vectorizer.fit_transform(data.utterances)
     return X, vectorizer
-
 
 
 def get_context_features(data, actions=None):
--- scripts/train_models.py
+++ scripts/train_models.py
@@ -4,6 +4,8 @@
 import os
 import argparse
 
+import numpy as np
+from scipy import sparse
 from sklearn.externals import joblib
 from sklearn.linear_model import LogisticRegression
 
@@ -15,7 +17,7 @@ from hrc_speech_prediction.models import ContextFilterModel
 
 TFIDF = False
 N_GRAMS = (1, 2)
-ADD_LAST_ACTION = False
+ADD_LAST_ACTION = True
 
 parser = argparse.ArgumentParser("Train and evaluate classifier")
 parser.add_argument('path', help='path to the experiment data',
@@ -24,29 +26,37 @@ args = parser.parse_args()
 
 data = TrainData.load(os.path.join(args.path, "train.json"))
 train_ids = [i for p in TRAIN_PARTICIPANTS for i in data.data[p].ids]
-if ADD_LAST_ACTION:
-    # Adds one data point for missing objects
-    last_action_ids = [data.data['15.ADT'][-1].ids[i] for i in (-4, -2)]
-    assert([data.get_pair_from_id(i)[0].label for i in last_action_ids
-            ] == ['front_2', 'front_4'])
-    train_ids.extend(last_action_ids)
 # Get features
 X_context, _ = get_context_features(data, actions=ALL_ACTIONS)
 X_context = X_context[train_ids, :]
-if ADD_LAST_ACTION:
-    # use neutral context for last actions
-    X_context[-2:, :] = .5
 X_speech, vocabulary = get_bow_features(data, tfidf=TFIDF, n_grams=N_GRAMS,
                                         max_features=None)
 X_speech = X_speech[train_ids, :]
 labels = [list(data.labels)[i] for i in train_ids]
+if ADD_LAST_ACTION:
+    X_s_new = vocabulary.transform([
+        "white part with red stripes in the middle",
+        "white cylindrical base with red lines in the middle",
+        "white part with red stripes spread appart",
+        "white cylindrical base with one red lines at the top and one at the bottom",
+    ])
+    X_speech = sparse.vstack((X_speech, X_s_new))
+    # use neutral context for last actions
+    X_c_new = np.tile(np.average(X_context, axis=0), (4, 1))
+    X_context = np.concatenate((X_context, X_c_new), axis=0)
+    labels = labels + ['front_2'] * 2 + ['front_4'] * 2
+    weights = np.ones(len(labels))
+    weights[-4:] = len(labels) * 1. / len(ALL_ACTIONS)
+else:
+    weights = None
 
 for features in ('speech', 'both'):
     model = ContextFilterModel(
         LogisticRegression(), ALL_ACTIONS, features=features,
         randomize_context=.3,
-    ).fit(X_context, X_speech, labels)
-    model_path = os.path.join(args.path, 'model_{}.pkl'.format(features))
+    ).fit(X_context, X_speech, labels, sample_weight=weights)
+    model_path = os.path.join(args.path, 'model_{}{}.pkl'.format(
+        features, '_table' if ADD_LAST_ACTION else ''))
     with open(model_path, "wb") as m:
         joblib.dump(model, m, compress=9)
 with open(os.path.join(args.path, 'vocabulary.pkl'), "wb") as m:
