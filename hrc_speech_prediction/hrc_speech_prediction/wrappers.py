5c310b68e68b44cdb6c4eebca9f06995beb4d2fd
Adam Wolnikowski <adam.wolnikowski@yale.edu>
2018-04-15 22:04:56 -0400

    began wrapper implementation

--- hrc_speech_prediction/hrc_speech_prediction/wrappers.py
+++ hrc_speech_prediction/hrc_speech_prediction/wrappers.py
@@ -5,16 +5,25 @@ Wrappers
 so that it can be used side by side with
 scikit vectorizers
 
-functions to implement:
+functions to implement: (functions that I found used in the hrc code)
 vectorizer.transform([speech])
 vectorizer.transfrom([utter])
 vectorizer.fit_transform(data.utterances)
 """
 
+import numpy as np
 from gensim.models import word2vec
 
 
-class Word2Vectorizer(object):
-    def transform():
+class Word2Vectorizer:
+    def fit(sentences):
+        self.model = word2vec.Word2Vec(sentences)
+        return self.model
 
-    def fit_transform():
+    def transform(sentences):
+        fit(sentences)
+        return self.model.syn0
+
+    def fit_transform(sentences):
+        fit(sentences)
+        return transform()
