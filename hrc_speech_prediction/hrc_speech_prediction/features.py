2fe5cecd6f626107e9519e83f3362c348c7968bf
Adam Wolnikowski <adam.wolnikowski@yale.edu>
2018-04-15 21:31:19 -0400

    added get_w2v_features() and created word2vec vectorizer wrapper, needs to be implemented

--- hrc_speech_prediction/hrc_speech_prediction/features.py
+++ hrc_speech_prediction/hrc_speech_prediction/features.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-
+from hrc_speech_prediction.wrappers import Word2Vectorizer
 
 def get_bow_features(data, tfidf=True, n_grams=(1, 1), max_features=None):
     if tfidf:
@@ -10,7 +10,6 @@ def get_bow_features(data, tfidf=True, n_grams=(1, 1), max_features=None):
                                      max_features=max_features)
     X = vectorizer.fit_transform(data.utterances)
     return X, vectorizer
-
 
 def get_context_features(data, actions=None):
     all_labels = list(set(data.labels)) if actions is None else actions
@@ -23,3 +22,8 @@ def get_context_features(data, actions=None):
                 # mark missing object after it's taken
                 X[trial_ids[(i + 1):], l] = 0
     return X, all_labels
+
+def get_w2v_features(data):
+    vectorizer = Word2Vectorizer()
+    X = vectorizer.fit_transform(data.utterances)
+    return X, vectorizer
---
+++ hrc_speech_prediction/hrc_speech_prediction/wrappers.py
@@ -0,0 +1,20 @@
+"""
+Wrappers
+
+- for gensim word2vec impelementation
+so that it can be used side by side with
+scikit vectorizers
+
+functions to implement:
+vectorizer.transform([speech])
+vectorizer.transfrom([utter])
+vectorizer.fit_transform(data.utterances)
+"""
+
+from gensim.models import word2vec
+
+
+class Word2Vectorizer(object):
+    def transform():
+
+    def fit_transform():
